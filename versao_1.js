c1ea4ccfabbcdf1b26a418b3dfbcdec37b264fa4
Rafael Simas <rafaelsimasdev@gmail.com>
2023-05-25 14:41:09 -0300

    Terminei o projeto blackjack semana 1 e 2 versão 1.0

--- versao_1.js
+++ versao_1.js
@@ -10,3 +10,55 @@
  * 
  * 
  */
+
+console.log("Boas vindas ao jogo de BlackJack!")
+
+let confirmacao = confirm("Quer iniciar uma nova rodada?")
+
+if(confirmacao){
+   
+
+   // const carta = comprarCarta();
+   // // Método que sorteia uma carta. Por exemplo, o rei de ouros 
+   
+   // console.log(carta.texto) 
+   // // imprime o texto da carta. Nesse caso: "K♦️" 
+   
+   // console.log(carta.valor) 
+   // // imprime o valor da carta (um número). Nesse caso: 10)
+
+   let usuarioPrimeiraCompra = comprarCarta()
+   let usuarioSegundaCompra = comprarCarta()
+
+   let maquinaPrimeiraCompra = comprarCarta()
+   let maquinaSegundaCompra = comprarCarta()
+
+   let pontuacaoUsuario = usuarioPrimeiraCompra.valor + usuarioSegundaCompra.valor
+   let pontuacaoMaquina = maquinaPrimeiraCompra.valor + maquinaSegundaCompra.valor
+
+   let valorUsuario = usuarioPrimeiraCompra.texto+` `+usuarioSegundaCompra.texto
+   let valorMaquina = maquinaPrimeiraCompra.texto+` `+maquinaSegundaCompra.texto
+
+   console.log(`Usuário - cartas: ${valorUsuario} - pontuação: ${pontuacaoUsuario} `)
+   console.log(`Computador - cartas: ${valorMaquina} - pontuação: ${pontuacaoMaquina}`)      
+
+   if(pontuacaoUsuario > pontuacaoMaquina){
+      console.log(`O usuário ganhou!`)
+   } else if(pontuacaoMaquina > pontuacaoUsuario){
+      console.log(`O computador ganhou!`)
+   } else{
+      console.log(`Empate!`)
+   }
+
+
+
+
+
+
+
+
+
+}else{
+   console.log("O jogo acabou")
+}
+
